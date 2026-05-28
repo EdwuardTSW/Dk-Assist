331186f235c391fff92c54eb1656d979648997d9
EdwuardTSW <edwuard2213dk@gmail.com>
2026-05-28 13:07:59 -0600

    feat: project structure and Cliente domain layer

---
+++ DkAssist/DkAssist.Application/Services/ClienteService.cs
@@ -0,0 +1,22 @@
+﻿using DkAssist.Domain.Interfaces;
+using DkAssist.Domain.Models;
+using System.Runtime.CompilerServices;
+
+namespace DkAssist.Application.Services
+{
+    public class ClienteService
+    {
+        private readonly IClienteRepository _repo;
+        public ClienteService(IClienteRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<Cliente> ObtenerTodos() => _repo.ObtenerTodos();
+        public Cliente? ObtenerPorId(int id) => _repo.ObtenerPorId(id);
+        public void Agregar(Cliente cliente) => _repo.Agregar(cliente);
+        public void Actualizar(Cliente cliente) => _repo.Actualizar(cliente);
+        public void Eliminar(int id) => _repo.Eliminar(id);
+
+    }
+}
---
+++ DkAssist/DkAssist.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using DkAssist.Domain.Models;
+
+namespace DkAssist.Domain.Interfaces
+{
+    public interface IClienteRepository
+    {
+        List<Cliente> ObtenerTodos();
+        Cliente? ObtenerPorId(int id);
+        void Agregar(Cliente cliente);
+        void Actualizar(Cliente cliente);
+        void Eliminar(int id);
+    }
+}
